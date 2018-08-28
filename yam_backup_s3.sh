12209433f4f73d9f133ecaa7bdfcdeb5f5fd93fc
Jon Leverrier <jon@youandme.digital>
2018-08-28 18:36:11 +0200

    removed hardcoded path and put into variable

--- yam_backup_s3.sh
+++ yam_backup_s3.sh
@@ -6,9 +6,11 @@
 
 # /bin/bash yam_backup_s3.sh yam yam-avalon-ams3-01
 
+PATH_BACKUP='yamdigital/servers/backups'
+
 # VARIABLES THAT ARE PASSED IN TO THE SCRIPT...
 # FOR EXAMPLE "yam_backup_s3.sh yam yam-avalon-ams3-01"
 USER=$1
 SERVER_FOLDER=$2
 
-s3cmd sync --delete-removed --skip-existing /home/$USER/backup/ s3://yamdigital/servers/backups/$SERVER_FOLDER/home/$USER/backup/
+s3cmd sync --delete-removed --skip-existing /home/$USER/backup/ s3://$PATH_BACKUP/$SERVER_FOLDER/home/$USER/backup/
