d69a3f9d3dfa45f5178c848ab69e88fe8ff27106
Jon Leverrier <jon@youandme.digital>
2018-08-28 18:03:52 +0200

    updated variables

--- yam_backup_local.sh
+++ yam_backup_local.sh
@@ -15,20 +15,20 @@
 # /home/user/public/website3
 
 # DEFAULT VARIABLES
-DATE_FULL=`date '+%Y-%m-%d'`
-BACKUP_DURATION_TIME='+6'
+YAM_DATEFORMAT_FULL=`date '+%Y-%m-%d'`
+YAM_BACKUP_DURATION='+6'
 
 # INLINE VARIABLES
 USER=$1
 
 # COLOURS
-RESTORE=$(echo -en '\033[0m')
-CYAN=$(echo -en '\033[00;36m')
-WHITE=$(echo -en '\033[01;37m')
+COLOUR_RESTORE=$(echo -en '\033[0m')
+COLOUR_CYAN=$(echo -en '\033[00;36m')
+COLOUR_WHITE=$(echo -en '\033[01;37m')
 
-echo "${WHITE}>>${RESTORE}"
-echo "${WHITE}>> Starting backup process for ${USER} ${RESTORE}"
-echo "${WHITE}>>${RESTORE}"
+echo "${COLOUR_WHITE}>>${COLOUR_RESTORE}"
+echo "${COLOUR_WHITE}>> Starting backup process for ${USER} ${COLOUR_RESTORE}"
+echo "${COLOUR_WHITE}>>${COLOUR_RESTORE}"
 
 # if backup folder for user exists skip, else create a folder called backup
 if [ -d "/home/${USER}/backup" ]; then
@@ -59,21 +59,21 @@ for d in /home/${USER}/public/*; do
           mkdir -p /home/${USER}/backup/${d##*/}/
 
           # dump database and place it in a tempory dir...
-          echo "${CYAN}-- Dumping database ${RESTORE}"
+          echo "${COLOUR_CYAN}-- Dumping database ${COLOUR_RESTORE}"
           mysqldump -u root yam_db_${USER}_${d##*/} > /home/${USER}/backup/temp/yam_db_${USER}_${d##*/}.sql
 
           # tar database and entire web folder...
-          echo "${CYAN}-- Compressing database and web folder ${RESTORE}"
-          tar -czf /home/${USER}/backup/${d##*/}/${USER}-${d##*/}-${DATE_FULL}.tar.gz /home/${USER}/backup/temp /home/${USER}/public/${d##*/}
+          echo "${COLOUR_CYAN}-- Compressing database and web folder ${COLOUR_RESTORE}"
+          tar -czf /home/${USER}/backup/${d##*/}/${USER}-${d##*/}-${YAM_DATEFORMAT_FULL}.tar.gz /home/${USER}/backup/temp /home/${USER}/public/${d##*/}
 
           # clean up data in temp folder...
-          echo "${CYAN}-- Cleaning up tempory folder ${RESTORE}"
+          echo "${COLOUR_CYAN}-- Cleaning up tempory folder ${COLOUR_RESTORE}"
           rm -rf /home/${USER}/backup/temp
 
           # delete old backups...
-          echo "${CYAN}-- Checking for old backups to delete ${RESTORE}"
+          echo "${COLOUR_CYAN}-- Checking for old backups to delete ${COLOUR_RESTORE}"
           if [ -d "/home/${USER}/backup/${d##*/}/" ]; then
-              find /home/${USER}/backup/${d##*/}/* -daystart -mtime ${BACKUP_DURATION_TIME} -exec rm {} \;
+              find /home/${USER}/backup/${d##*/}/* -daystart -mtime ${YAM_BACKUP_DURATION} -exec rm {} \;
           fi
 
       fi
@@ -82,4 +82,4 @@ for d in /home/${USER}/public/*; do
   fi
 done
 
-echo "${WHITE}>> Backup for ${USER} complete.${RESTORE}"
+echo "${COLOUR_WHITE}>> Backup for ${USER} complete.${COLOUR_RESTORE}"
